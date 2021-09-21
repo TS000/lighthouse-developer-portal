fcbe53d94c2b1e600f68a78bed38cd903652e98e
Nick Luckey <nickluckey@gmail.com>
2021-09-21 17:38:40 +0000

    Changed start up script

--- .devcontainer/start-up.sh
+++ .devcontainer/start-up.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 oryx build -p virtualenv_name=.venv --log-file /tmp/oryx-build.log --manifest-dir /tmp || echo 'Could not auto-build. Skipping.'
-pip install pre-commit && pre-commit install && pre-commit
+pip install pre-commit && pre-commit install
+cd /workspaces/lighthouse-backstage && pre-commit
