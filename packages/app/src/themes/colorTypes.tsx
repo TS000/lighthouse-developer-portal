d2d3ca06e0f316c9d95c6f46fd0ee3efa0d413a7
Brent Schneider <brent@milkshakeinteractive.com>
2021-09-27 14:30:54 -0700

    Added VADS secondary color ramps to the theme colors (#158)
    * added VADS secondary to the mix, now all the base VADS colors are documented.
    
    * amending colors
    
    * Apply suggestions from code review
    
    Nice catch on the typo —
    
    Co-authored-by: Kaemon Lovendahl <KaemonLovendahl@outlook.com>
    
    Co-authored-by: Kaemon Lovendahl <KaemonLovendahl@outlook.com>

--- packages/app/src/themes/colorTypes.tsx
+++ packages/app/src/themes/colorTypes.tsx
@@ -4,6 +4,11 @@ import { yellow } from "@material-ui/core/colors";
 export const myYellow = yellow;
 
 // This is where we can define our own color's hue[shades]
+
+
+/**
+ * VADS Primary and Alt
+ */
 const vadsBlue = {
     50: '#E1F3F8',  //$color-primary-alt-lightest
     100: '#E1F3F8', 
@@ -15,10 +20,10 @@ const vadsBlue = {
     700: '#046B99', //$color-primary-alt-darkest
     800: '#003E73', //$color-primary-darker
     900: '#112E51', //$color-primary-darkest
-    A100: '#000',
-    A200: '#000',
-    A400: '#000',
-    A700: '#000'
+    A100: '#CBE9F4',
+    A200: '#6EC9EB',
+    A400: '#159ABD',
+    A700: '#115573',
 };
 
 export const blue = {
@@ -38,56 +43,61 @@ export const blue = {
     A700: vadsBlue.A700,
 };
 
-
-const vadsGold = {
-    50: '#FFF1D2',  //$color-gold-lightest
-    100: '#FAD980', 
-    200: '#FAD980', //$color-gold-lighter 
-    300: '#F9C642', //$color-gold-light
-    400: '#FCB02F',
-    500: '#FDB81E', //$color-gold
-    600: '#F5AE31',
-    700: '#E8A937',
-    800: '#C99E48',
-    900: '#988530', //$color-gold-darker
-    A100: '#FFF1D2',
-    A200: '#FAD980',
-    A400: '#F9C642',
-    A700: '#FDB81E'
+/**
+ * VADS Sedondary
+ */
+const vadsRed = {
+    50: '#FEF9F9',
+    100: '#F9DEDE',
+    200: '#EFBCBC',
+    300: '#E59393',
+    400: '#E07E7E',
+    500: '#D65555',
+    600: '#E31C3D',
+    700: '#CD2026',
+    800: '#981B1E',
+    900: '#7A0609',
+    A100: '#FFF8F8',
+    A200: '#F7CECE',
+    A400: '#C83030',
+    A700: '#6D1315',
 };
 
-export const gold = {
-    50: vadsGold[50],
-    100: vadsGold[100],
-    200: vadsGold[200],
-    300: vadsGold[300],
-    400: vadsGold[400],
-    500: vadsGold[500],
-    600: vadsGold[600],
-    700: vadsGold[700],
-    800: vadsGold[800],
-    900: vadsGold[900],
-    A100: vadsGold.A100,
-    A200: vadsGold.A200,
-    A400: vadsGold.A400,
-    A700: vadsGold.A700,
+export const red = {
+    50: vadsRed[50],
+    100: vadsRed[100],
+    200: vadsRed[200],
+    300: vadsRed[300],
+    400: vadsRed[400],
+    500: vadsRed[500],
+    600: vadsRed[600],
+    700: vadsRed[700],
+    800: vadsRed[800],
+    900: vadsRed[900],
+    A100: vadsRed.A100,
+    A200: vadsRed.A200,
+    A400: vadsRed.A400,
+    A700: vadsRed.A700,
 };
 
+/**
+ * VADS Gray
+ */
 const vadsGray = {
-    50: '#f1f1f1',  
-    100: '#f1f1f1', //$color-gray-lightest
-    200: '#d6d7d9', //$color-gray-lighter 
-    300: '#e4e2e0', //$color-gray-warm-light
-    400: '#dce4ef', //$color-gray-cool-light
-    500: '#aeb0b5', //$color-gray-light
-    600: '#757575', //$color-gray-medium
-    700: '#E8A937', //$color-gray
-    800: '#494440', //$color-gray-warm-dark
-    900: '#323a45', //$color-gray-dark
-    A100: '#323a45',
-    A200: '#323a45',
-    A400: '#323a45',
-    A700: '#323a45'
+    50: '#F1F1F1',  //$color-gray-lightest
+    100: '#E4E2E0', //$color-gray-warm-light 
+    200: '#D6D7D9', //$color-gray-lighter
+    300: '#DCE4EF', //$color-gray-cool-light
+    400: '#AEB0B5', //$color-gray-light
+    500: '#757575', //$color-gray-medium
+    600: '#5B616B', //$color-gray
+    700: '#494440', //$color-gray-warm-dark
+    800: '#323A45', //$color-gray-dark
+    900: '#07080A', 
+    A100: '#F1F1F1',
+    A200: '#DCDCDB',
+    A400: '#CCCCCC',
+    A700: '#535559'
 };
 
 export const gray = {
@@ -108,9 +118,88 @@ export const gray = {
 };
 
 /**
- * Variable for common colors like white/black
+ * VADS Gold
+ */
+const vadsGold = {
+    50: '#FEFAEF',  
+    100: '#FFF1D2', //$color-gold-lightest
+    200: '#FAD980', //$color-gold-lighter 
+    300: '#F9C642', //$color-gold-light
+    400: '#FDC23E',
+    500: '#FDB81E', //$color-gold
+    600: '#ECB22F',
+    700: '#D5B36F',
+    800: '#C99E48',
+    900: '#988530', //$color-gold-darker
+    A100: '#FFE9B8',
+    A200: '#EDD38D',
+    A400: '#E0BC5B',
+    A700: '#E7A82A'
+};
+
+export const gold = {
+    50: vadsGold[50],
+    100: vadsGold[100],
+    200: vadsGold[200],
+    300: vadsGold[300],
+    400: vadsGold[400],
+    500: vadsGold[500],
+    600: vadsGold[600],
+    700: vadsGold[700],
+    800: vadsGold[800],
+    900: vadsGold[900],
+    A100: vadsGold.A100,
+    A200: vadsGold.A200,
+    A400: vadsGold.A400,
+    A700: vadsGold.A700,
+};
+
+/**
+ * VADS Green
+ */
+const vadsGreen = {
+    50: '#FCFAFA',
+    100: '#F1F1F1', //$color-green-lightest
+    200: '#E4E2E0',
+    300: '#D6D7D9', //$color-green-lighter
+    400: '#7AA688',
+    500: '#AEB0B5', //$color-green-light
+    600: '#757575', //$color-green
+    700: '#5B616B',
+    800: '#494440', //$color-green-darker
+    900: '#323A45',
+    A100: '#F1F1F1',
+    A200: '#DCDCDB',
+    A400: '#CCCCCC',
+    A700: '#535559',
+};
+
+export const green = {
+    50: vadsGreen[50],
+    100: vadsGreen[100],
+    200: vadsGreen[200],
+    300: vadsGreen[300],
+    400: vadsGreen[400],
+    500: vadsGreen[500],
+    600: vadsGreen[600],
+    700: vadsGreen[700],
+    800: vadsGreen[800],
+    900: vadsGreen[900],
+    A100: vadsGreen.A100,
+    A200: vadsGreen.A200,
+    A400: vadsGreen.A400,
+    A700: vadsGreen.A700,
+};
+
+
+/**
+ * VADS Misc for common colors
  */
 export const common = {
+    base: "#212121",
+    linkColorDefault: "#004795",
+    linkColorVisited: "#4c2c92",
+    linkColorFocus: "#3e94cf",
     white: "#FFFFFF",
     black: "#212121"
 }
