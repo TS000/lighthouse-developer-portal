e5c424da0db90d04a254aa995be5437a78b7757c
Kaemon Lovendahl <KaemonLovendahl@outlook.com>
2021-12-03 11:20:49 -0700

    Add custom nginx pages to dockerfile (#268)
    * Add custom nginx pages to dockerfile
    
    * Actually plugin the GA Analytics code

--- packages/app/src/apis.ts
+++ packages/app/src/apis.ts
@@ -3,13 +3,25 @@ import {
   scmIntegrationsApiRef,
   ScmAuth,
 } from '@backstage/integration-react';
-import { AnyApiFactory, configApiRef, createApiFactory } from '@backstage/core-plugin-api';
+import {
+  AnyApiFactory,
+  configApiRef,
+  createApiFactory,
+  analyticsApiRef,
+} from '@backstage/core-plugin-api';
+import { GoogleAnalytics } from '@backstage/plugin-analytics-module-ga';
 
 export const apis: AnyApiFactory[] = [
   createApiFactory({
     api: scmIntegrationsApiRef,
     deps: { configApi: configApiRef },
     factory: ({ configApi }) => ScmIntegrationsApi.fromConfig(configApi),
+  }),
+  // Instantiate and register the GA Analytics API Implementation.
+  createApiFactory({
+    api: analyticsApiRef,
+    deps: { configApi: configApiRef },
+    factory: ({ configApi }) => GoogleAnalytics.fromConfig(configApi),
   }),
   ScmAuth.createDefaultApiFactory(),
 ];
