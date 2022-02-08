07f4d546f5f3d51f6d3d9b4f7550df3379e68db4
Kaemon Lovendahl <KaemonLovendahl@outlook.com>
2022-02-08 10:49:10 -0700

    Add links to config (#399)
    * Add links to config
    
    * Add test frontend config
    
    * Add better icons
    
    * Try material ui icons
    
    * Remove test name
    
    * Remove test cuz it is useless
    
    * Remove explore page test

--- packages/app/cypress/integration/explorePage.js
+++
@@ -1,8 +0,0 @@
-describe('Explore Page', () => {
-  it('should render the explore page', () => {
-    window.localStorage.setItem('@backstage/core:SignInPage:provider', 'guest');
-    cy.visit('/');
-    cy.get('h6').contains('Plugins').click();
-    cy.contains('Explore the Embark ecosystem');
-  });
-});
