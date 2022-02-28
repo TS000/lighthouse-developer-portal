fd7721299f35c75ea59639f7e22f0afbe9136f2e
Nick Luckey <nickluckey@gmail.com>
2022-02-28 14:02:20 -0800

    [API-13780] Updates to dismissable banner text (#459)

--- packages/app/src/components/banner/Banner.tsx
+++ packages/app/src/components/banner/Banner.tsx
@@ -5,7 +5,7 @@ export const Banner = () => (
   <DismissableBanner
     variant="info"
     id="beta_dismissable"
-    message="The Lighthouse Embark application is currently in beta development and not in production."
+    message="The Lighthouse developer portal is currently in beta development and not in production."
     fixed
   />
 );
