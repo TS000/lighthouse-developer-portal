39ce4cb0f658357c47aba79e3050f45b97ab431b
Muhammad Abdusamad <muhammadabdusamad@yahoo.com>
2022-03-21 10:13:36 -0400

    Delete workflow (#547)
    * [API-14255] updating token
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * [API-14255] updating token
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * [API-14255] updating token
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * [API-14255] updating token
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * [API-14255] updating token
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * [API-14255] updating token
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * [API-14255] updating token
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * [API-14255] updating token
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * [API-14255] updating token
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * [API-14255] updating token
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * [API-14255] updating token
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * [API-14255] updating token
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * [API-14255] removed yaml
    
    Signed-off-by: GitHub <noreply@github.com>

--- scripts/delete_workflows.js
+++ scripts/delete_workflows.js
@@ -1,7 +1,9 @@
 const { Octokit } = require('@octokit/core');
+const core = require('@actions/core');
+
 const octokit = new Octokit({
   auth: process.env.WORKFLOW_TOKEN,
-});
+})
 
 function printError(error) {
   const {
@@ -16,48 +18,52 @@ function printError(error) {
 
 async function deleteWorkflows(owner, repo) {
   // Get total amount of workflows
-  const {
-    data: { total_count },
-  } = await octokit.request('GET /repos/{owner}/{repo}/actions/runs', {
-    owner,
-    repo,
-  });
+  try {
+    const {
+      data: { total_count },
+    } = await octokit.request('GET /repos/{owner}/{repo}/actions/runs', {
+      owner,
+      repo,
+    });
 
-  // Get the current page or runs to delete
-  let pageToDelete = Math.ceil(total_count / 25);
+    // Get the current page or runs to delete
+    let pageToDelete = Math.ceil(total_count / 25);
 
-  // Delete from the oldest workflows until there are only 5 of the most recent pages left
-  while (pageToDelete > 5) {
-    try {
-      const {
-        data: { total_count, workflow_runs },
-      } = await octokit.request('GET /repos/{owner}/{repo}/actions/runs', {
-        owner,
-        repo,
-        per_page: 25,
-        page: pageToDelete,
-      });
-      pageToDelete = Math.ceil(total_count / 25);
-      workflow_runs.forEach(async ({ id }) => {
-        try {
-          await octokit.request(
-            'DELETE /repos/{owner}/{repo}/actions/runs/{run_id}',
-            {
-              owner,
-              repo,
-              run_id: id,
-            },
-          );
-          console.log(`Deleted workflow ${id}`);
-        } catch (error) {
-          printError(error);
-        }
-      });
-    } catch (error) {
-      printError(error);
+    // Delete from the oldest workflows until there are only 5 of the most recent pages left
+    while (pageToDelete > 5) {
+      try {
+        const {
+          data: { total_count, workflow_runs },
+        } = await octokit.request('GET /repos/{owner}/{repo}/actions/runs', {
+          owner,
+          repo,
+          per_page: 25,
+          page: pageToDelete,
+        });
+        pageToDelete = Math.ceil(total_count / 25);
+        workflow_runs.forEach(async ({ id }) => {
+          try {
+            await octokit.request(
+              'DELETE /repos/{owner}/{repo}/actions/runs/{run_id}',
+              {
+                owner,
+                repo,
+                run_id: id,
+              },
+            );
+            console.log(`Deleted workflow ${id}`);
+          } catch (error) {
+            printError(error);
+          }
+        });
+      } catch (error) {
+        printError(error);
+      }
+      // Waiting a second between deleting pages of workflows (25 workflows per page)
+      await new Promise(resolve => setTimeout(resolve, 1000));
     }
-    // Waiting a second between deleting pages of workflows (25 workflows per page)
-    await new Promise(resolve => setTimeout(resolve, 1000));
+  } catch (error) {
+    core.setFailed(error.message)
   }
 }
 deleteWorkflows('department-of-veterans-affairs', 'lighthouse-developer-portal');
